6683eda3b1d6d5b147f26de6da4e253943f76cff
someshj5 <someshj5@gmail.com>
2022-05-26 17:53:07 +0530

    apis updated

--- demo/urls.py
+++ demo/urls.py
@@ -1,7 +1,8 @@
 from django import views
 from django.urls import path
-from .views import Call
+from .views import Call,PlayAudio
 
 urlpatterns = [
     path('calls',Call),
+    path('play',PlayAudio),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework.response import Response
 from django.conf import settings
 from twilio.twiml.voice_response import VoiceResponse,Say
@@ -19,11 +18,12 @@ def get_custom_response(success=False, message='something went wrong', data=None
 def Call(request):
     # Get phone number we need to call
     try:
-        url=None
-        phone_number = requests.post(url)
+        url=None #url of the api phone_no from the CRM database's 
+        response = requests.get(url)
+        phone_number = response.get('phone_number')
+
     except Exception as e:
-        # raise e
-        phone_number = request.GET.get('phoneNumber', 8286739482)
+        phone_number =  XXXXXXXXXXX
 
 
     if not phone_number:
@@ -40,15 +40,25 @@ def Call(request):
     try:
         res = twilio_client.calls.create(from_=settings.TWILIO_CALLER_ID,
                                    to=phone_number,
-                                #    url='https://b482-117-96-242-84.in.ngrok.io/outbound'
+                                #    url='https://b482-117-96-242-84.in.ngrok.io/play'
                                    url='http://demo.twilio.com/docs/voice.xml',
                                                )
 
         print('----------->',res.sid)
+        msg="Incoming Call!"
+        return get_custom_response(success=True,message=msg,status=200)
     
     except Exception as e:
-        # app.logger.error(e)
         message = e.msg if hasattr(e, 'msg') else str(e)
         return get_custom_response(message=message)
-    msg="Incoming Call!"
-    return get_custom_response(message=msg)
+
+def PlayAudio(request):
+    try:
+        response = VoiceResponse()
+        url='path_of_audio_clip'
+        response.play(url)
+        response.hangup()
+        return get_custom_response(success=True)
+    except Exception as e:
+        message = e.msg if hasattr(e, 'msg') else str(e)
+        return get_custom_response(message=message)
